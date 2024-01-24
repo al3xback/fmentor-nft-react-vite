179cab253383ab875daa50aad4762317f884c64f
alex <al3xback@gmail.com>
2024-01-24 07:18:30 +0700

    fixed title

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -3,7 +3,7 @@ import './Header.scss';
 const Header = () => {
 	return (
 		<header>
-			<h1 className="sr-only">NFT</h1>
+			<h1 className="sr-only">NFT preview card component</h1>
 		</header>
 	);
 };
